03e608465a3cefd58cc262458ba0fdb63c86f79b
umairx97 <umair.bajwa@hotmail.com>
2020-03-25 21:39:49 +0500

    check build

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 import Test from './components/test/Test.js'
 
 const App = () => {
-  return <div> <Test /></div>
+  return <div> TestComponent: <Test /></div>
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
